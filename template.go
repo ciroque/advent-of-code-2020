c6b2b673b8a9f8c0afa56049b1981b81aa2dd375
steve.wagner <steve.wagner@termly.io>
2020-12-04 17:53:01 -0800

    Adds full file read logic to template.go

--- template.go
+++ template.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"sync"
@@ -44,6 +45,12 @@ func LoadPuzzleInput() string {
 		panic(fmt.Sprintf("open %s: %v", filename, err))
 	}
 	defer fd.Close()
+
+	scanner := bufio.NewScanner(fd)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
 
 	return ""
 }
