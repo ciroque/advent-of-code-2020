b38cfb42abf76edb5583bc166faf583edfc88ca8
steve.wagner <steve.wagner@termly.io>
2020-12-06 12:00:54 -0800

    Better file read support

--- support/filereader.go
+++ support/filereader.go
@@ -6,11 +6,15 @@ import (
 	"strings"
 )
 
-func ReadFile(filename string) []string {
+func ReadFile(filename string) string {
 	content, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(fmt.Errorf("failed to open '%s', %v", filename, err))
 	}
 
-	return strings.Split(string(content), "\n")
+	return string(content)
+}
+
+func ReadFileIntoLines(filename string) []string {
+	return strings.Split(ReadFile(filename), "\n")
 }
