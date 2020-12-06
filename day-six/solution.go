76664e4573e1113d05828e181721a813200106cf
steve.wagner <steve.wagner@termly.io>
2020-12-06 14:52:51 -0800

    Completes day six part two

--- day-six/solution.go
+++ day-six/solution.go
@@ -30,7 +30,6 @@ func main() {
 }
 
 func DoExamples(waitGroup *sync.WaitGroup) {
-
 	waitGroup.Done()
 }
 
@@ -57,7 +56,45 @@ func DoPartOne(channel chan int, waitGroup *sync.WaitGroup) {
 }
 
 func DoPartTwo(channel chan int, waitGroup *sync.WaitGroup) {
-	channel <- 2
+	puzzleInput := LoadPuzzleInput()
+	parsedPuzzleInput := strings.Split(puzzleInput, "\n")
+
+	processResponse := func(response string, answers map[int32]int) {
+		for _, question := range response {
+			if _, found := answers[question]; found {
+				answers[question] = answers[question] + 1
+			} else {
+				answers[question] = 1
+			}
+		}
+	}
+
+	checkResponses := func(count int, answers map[int32]int) int {
+		sum := 0
+		for _, v := range answers {
+			if v == count {
+				sum = sum + 1
+			}
+		}
+
+		return sum
+	}
+
+	sum := 0
+	count := 0
+	answers := map[int32]int{}
+	for _, response := range parsedPuzzleInput {
+		if len(response) == 0 {
+			sum = sum + checkResponses(count, answers)
+			answers = map[int32]int{}
+			count = 0
+		} else {
+			count = count + 1
+			processResponse(response, answers)
+		}
+	}
+
+	channel <- sum
 	waitGroup.Done()
 }
 
