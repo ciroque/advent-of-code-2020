3d8c01f6bc4c6dea31e80a6b0db040289884d513
steve.wagner <steve.wagner@termly.io>
2020-12-13 21:43:13 -0800

    Completes day eight

--- day-eight/solution.go
+++ day-eight/solution.go
@@ -9,6 +9,42 @@ import (
 	"sync"
 )
 
+type Instruction struct {
+	Op         string
+	Arg        int
+	OriginalOp string
+}
+
+func (i *Instruction) swapOp() {
+	if i.Op == "jmp" {
+		i.Op = "nop"
+	} else if i.Op == "nop" {
+		i.Op = "jmp"
+	}
+}
+
+func (i *Instruction) restoreOp() {
+	i.Op = i.OriginalOp
+}
+
+func BuildInstructionList(puzzleInput []string) []Instruction {
+	parseLine := func(line string) Instruction {
+		parts := strings.Split(line, " ")
+		argument, _ := strconv.ParseInt(parts[1], 0, 0)
+		return Instruction{
+			Op:         parts[0],
+			Arg:        int(argument),
+			OriginalOp: parts[0],
+		}
+	}
+
+	var instructions []Instruction
+	for _, line := range puzzleInput {
+		instructions = append(instructions, parseLine(line))
+	}
+	return instructions
+}
+
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
@@ -38,46 +74,90 @@ func doExamples(waitGroup *sync.WaitGroup) {
 
 func doPartOne(channel chan int, waitGroup *sync.WaitGroup) {
 	puzzleInput := loadPuzzleInput()
+	instructions := BuildInstructionList(puzzleInput)
+	_, accumulator, cycle := executeFrom(instructions, 0)
+	if cycle == true {
+		log.Info().Msg("Cycle detected")
+	}
+	channel <- accumulator
+	waitGroup.Done()
+}
 
-	instructions := map[int]int{}
-	accumulator := 0
-	for index := range puzzleInput {
-		instructions[index] = 0
+func doPartTwo(channel chan int, waitGroup *sync.WaitGroup) {
+	puzzleInput := loadPuzzleInput()
+	instructions := BuildInstructionList(puzzleInput)
+
+	speculate := func(ops []int) (bool, int) {
+		acc := 0
+		foundProgramEnd := false
+		for _, ip := range ops {
+			instructions[ip].swapOp()
+			_, acc, foundProgramEnd = executeFrom(instructions, 0)
+			if foundProgramEnd {
+				return true, acc
+			}
+			instructions[ip].restoreOp()
+		}
+
+		return false, 0
 	}
 
-	instructionPointer := 0
-	for true {
-		line := puzzleInput[instructionPointer]
-		op, arg := parseLine(line)
+	foundProgramEnd := false
+	var accumulator int
+	nops := findOps(instructions, "nop")
 
-		if op == "acc" {
-			accumulator = accumulator + arg
-			instructionPointer = instructionPointer + 1
-		} else if op == "jmp" {
-			instructionPointer = instructionPointer + arg
-		} else {
-			instructionPointer = instructionPointer + 1
-		}
-
-		instructions[instructionPointer]++
-		if instructions[instructionPointer] > 1 {
-			break
-		}
+	foundProgramEnd, accumulator = speculate(nops)
+	if !foundProgramEnd {
+		jmps := findOps(instructions, "jmp")
+		foundProgramEnd, accumulator = speculate(jmps)
 	}
 
 	channel <- accumulator
 	waitGroup.Done()
 }
 
-func doPartTwo(channel chan int, waitGroup *sync.WaitGroup) {
-	channel <- 2
-	waitGroup.Done()
+// returns int, int, bool
+// The instruction pointer
+// The accumulator
+// true if a instruction length + 1 found, false otherwise
+func executeFrom(instructions []Instruction, instructionPointer int) (int, int, bool) {
+	instructionCounts := map[int]int{}
+	for index := range instructions {
+		instructionCounts[index] = 0
+	}
+
+	accumulator := 0
+	for true {
+		instruction := instructions[instructionPointer]
+
+		if instruction.Op == "acc" {
+			accumulator = accumulator + instruction.Arg
+			instructionPointer = instructionPointer + 1
+		} else if instruction.Op == "jmp" {
+			instructionPointer = instructionPointer + instruction.Arg
+		} else {
+			instructionPointer = instructionPointer + 1
+		}
+
+		instructionCounts[instructionPointer]++
+		if instructionCounts[instructionPointer] > 1 {
+			return instructionPointer, accumulator, false
+		} else if instructionPointer == len(instructions) {
+			return instructionPointer, accumulator, true
+		}
+	}
+
+	return instructionPointer, accumulator, false
 }
 
-func parseLine(line string) (string, int) {
-	parts := strings.Split(line, " ")
-	argument, _ := strconv.ParseInt(parts[1], 0, 0)
-	return parts[0], int(argument)
+func findOps(instructions []Instruction, op string) []int {
+	var ops []int
+	for index, instruction := range instructions {
+		if strings.Index(instruction.Op, op) == 0 {
+			ops = append(ops, index)
+		}
+	}
+	return ops
 }
 
 func loadPuzzleInput() []string {
